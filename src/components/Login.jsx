6467f11d115473751e0606877ca1a8bc2c3c6fa0
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-12-02 16:49:47 +0530

    integrated login api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,81 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://localhost:3000/auth/login', {
+                emailId, password
+            }, { withCredentials: true })
+        } catch (err) {
+            console.log("Err @ login : ", err?.message);
+        }
+    }
     return (
         <div className="card card-border bg-neutral w-96 mx-auto">
             <div className="card-body">
                 <h2 className="card-title justify-center text-2xl">Login</h2>
-                <div className='my-6 space-y-8'>
-                    <div>
-                        <label className="input validator input-secondary input-md">
-                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2.5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                >
-                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                                </g>
-                            </svg>
-                            <input type="email" placeholder="mail@site.com"
-                                required />
-                        </label>
-                        <div className="validator-hint hidden">Enter valid email address</div>
+                <form onSubmit={handleLogin}>
+                    <div className='my-6 space-y-8'>
+                        <div>
+                            <label className="input validator input-secondary input-md">
+                                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                        strokeWidth="2.5"
+                                        fill="none"
+                                        stroke="currentColor"
+                                    >
+                                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                    </g>
+                                </svg>
+                                <input type="email" placeholder="email ID" onChange={(e) => setEmailId(e.target.value)}
+                                    required />
+                            </label>
+                            <div className="validator-hint hidden">Enter valid email address</div>
+                        </div>
+                        <div>
+                            <label className="input validator input-secondary input-md">
+                                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                        strokeWidth="2.5"
+                                        fill="none"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                                        ></path>
+                                        <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                                    </g>
+                                </svg>
+                                <input
+                                    type="password"
+                                    required
+                                    placeholder="Password"
+                                    minlength="8"
+                                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                    title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </label>
+                            <p className="validator-hint hidden">
+                                Must be more than 8 characters, including
+                                <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <label className="input validator input-secondary input-md">
-                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <g
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2.5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
-                                    ></path>
-                                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-                                </g>
-                            </svg>
-                            <input
-                                type="password"
-                                required
-                                placeholder="Password"
-                                minlength="8"
-                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-                            />
-                        </label>
-                        <p className="validator-hint hidden">
-                            Must be more than 8 characters, including
-                            <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
-                        </p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary" type='submit'>Submit</button>
                     </div>
-                </div>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Submit</button>
-                </div>
+                </form>
             </div>
         </div>
     )
