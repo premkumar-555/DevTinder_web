dd8cc65ad93c85b7a3dc6e959929b1fe0c50fd4c
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-12-07 17:29:57 +0530

    fixed UserCard path import issue

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import UserCard from "./Feed/userCard";
+import React, { useState } from "react";
+import UserCard from "./Feed/UserCard";
 import { useDispatch, useSelector } from "react-redux";
 import { PROFILE_URL } from "../utils/constants";
 import { addUser } from "../redux/userSlice";
