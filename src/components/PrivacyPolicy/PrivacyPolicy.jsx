e13e9354fff0d65e06c614a89edc27ccf698f43d
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-12-12 15:52:10 +0530

    resolved template component import issue

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { data } from './privacyPolicies'
-import Template from '../Terms&Conditions/template'
+import Template from '../Terms&Conditions/Template'
 const PrivacyPolicy = () => {
     return (
         <Template page='Privacy Policies' data={data} />
