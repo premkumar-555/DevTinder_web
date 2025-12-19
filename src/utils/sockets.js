cdf30f034a169f9e89d748e7e39fe36d85c4eb7a
Premkumar Hulikoppe <premuhulikoppe@gmail.com>
2025-12-19 23:06:38 +0530

    fixed socket client inits for namespaces trail 1

--- src/utils/sockets.js
+++ src/utils/sockets.js
@@ -11,19 +11,30 @@ const socketConfigOptions = {
 };
 
 // Handle dynamic config options for cloud host
-if (location.hostname !== "localhost") {
-  socketConfigOptions.path = "/api/socket.io/";
-}
+const getSocketUrl = (namespace) => {
+  if (location.hostname !== "localhost") {
+    socketConfigOptions.path = "/api/socket.io/";
+  }
+};
 
 // socket client creator
-const createSocket = (url, token) => {
+const createSocket = (namespace, token) => {
+  // Default socket url dynamic for both local/cloud host platforms
+  let socketUrl = `${SOCKET_URL}`;
   socketConfigOptions.auth.token = token;
-  return io(url, socketConfigOptions);
+  // Handle socket urls for local or cloud hosts
+  // 1. If cloud host, update config path with namespace
+  if (location.hostname !== "localhost") {
+    // on cloud host socket io runs on www.xyzdomain.com/api/socket.io/
+    socketConfigOptions.path = `/api/socket.io/${namespace}`;
+  } else {
+    socketUrl = `${socketUrl}/${namespace}`;
+  }
+  return io(socketUrl, socketConfigOptions);
 };
 
 // Main namespace
-export const mainSocket = (token) => createSocket(`${SOCKET_URL}/`, token);
+export const mainSocket = (token) => createSocket("", token);
 
 // Request namespace
-export const requestSocket = (token) =>
-  createSocket(`${SOCKET_URL}/requests`, token);
+export const requestSocket = (token) => createSocket("requests", token);
